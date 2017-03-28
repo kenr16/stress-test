15c44c5360646bced1ea98e47a8bb30cc3fb8ab6
James Higgins and Ken Rutan <james@>
2017-03-28 12:00:23 -0700

    add checkboxes for 3 questions and add scripts code to show answers checked

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+  $("form#stress_survey").submit(function(event){
+    event.preventDefault();
+    $("#pos-responses").show();
+    var totalArray = [];
+    $("input:checkbox[name=pos-event]:checked").each(function(){
+      var posResponses = $(this).val();
+      totalArray.push(posResponses);
+      $('#pos-responses').append(posResponses + "<br>");
+    });
+
+    $("#neg-responses").show();
+    $("input:checkbox[name=neg-event]:checked").each(function(){
+      var negResponses = $(this).val();
+      totalArray.push(negResponses);
+      $('#neg-responses').append(negResponses + "<br>");
+    });
+    $('#stress_survey').hide();
+
+    var symptomArray = [];
+    $("#sym-responses").show();
+    $("input:checkbox[name=symptom]:checked").each(function(){
+      var symResponses = $(this).val();
+      symptomArray.push(symResponses);
+      $('#sym-responses').append(symResponses + "<br>");
+    });
+    $('#stress_survey').hide();
+
+
+
+
+
+  //   var userResponses = [];
+  //   $("input:checkbox[name=work-transportation]:checked").each(function(){
+  //     var workTransportationMode = $(this).val();
+  //     userResponses.push(workTransportationMode);
+  //   });
+  //   alert(userResponses);
+  //
+  //
+  //   $("input:checkbox[name=work-transportation]:checked").each(function(){
+  //    var workTransportationMode = $(this).val();
+  //    var capitalWorkTransportationMode = workTransportationMode.toUpperCase();
+  //
+  //    alert(capitalWorkTransportationMode);
+  //  });
+
+
+
+  });
+});
